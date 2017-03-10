ad5eef6cd7f48f9c0d473f2613291e7338c6b642
Athena1027 <athena_liu1027@foxmail.com>
2017-03-10 23:40:10 +0800

    Athena's homework

---
+++ Tasks and Projects/Tasks/Task 0- Learn to use Github (Hello World)/Athena.java
@@ -0,0 +1,11 @@
+public class ITCkubDemo {
+
+          public static void main(String[] args) {                     
+                   // TODO Auto-generated method stub
+     String name = "Athena";
+     int classNum = 24;
+     System.out.printIn("I am " + name + " in class " + classNum);
+     System.out.print("Hello World");
+              }
+       
+}
